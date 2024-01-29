8a662cf65b707ecb0f6734e040669579d728350c
Derek Fodeke-Rodgers <dmonarch134@gmail.com>
2024-01-29 08:45:35 -0600

    Auth fix

--- src/authentication/auth.ts
+++ src/authentication/auth.ts
@@ -1,26 +1,27 @@
 import {Env} from "../interfaces";
-
-const hasValidHeader=(request: Request, env: Env): boolean => {
-    const requestHeader = request.headers.get('X-Auth-Key');
-    if (!requestHeader || env.SHARED_SECRET ) {
+const hasValidHeader= (request: Request, env: Env): boolean => {
+    const requestHeader = request.headers.get('Authorization');
+    if (!requestHeader|| !env.SHARED_SECRET) {
         return false;
     }
-    const encoder = new TextEncoder();
-    const internalSecretKey = encoder.encode(env.SHARED_SECRET);
-    const userSecretKey = encoder.encode(requestHeader);
 
+    const encoder = new TextEncoder();
+    const userSecretKey = encoder.encode(requestHeader);
+    const internalSecretKey = encoder.encode(env.SHARED_SECRET);
+
+    //ensure the two keys are the same length
     if (userSecretKey.length !== internalSecretKey.length) {
         return false;
     }
-
-    return crypto.subtle.timingSafeEqual(userSecretKey, internalSecretKey);
+//compare the two keys in a timing-safe manner
+return crypto.subtle.timingSafeEqual(userSecretKey, internalSecretKey);
 }
 
-export function authorizeRequest(request: Request, env: Env): boolean {
-    const validMethods = ['POST', 'GET'];
-    if(validMethods.includes(request.method)) {
+export  function authorizeRequest(request: Request, env: Env): boolean{
+    const validMethods = ['GET', 'POST', 'PUT' ];
+    if (validMethods.includes(request.method)){
         return hasValidHeader(request, env);
     }
 
-    return hasValidHeader(request, env);
+    return false
 }
